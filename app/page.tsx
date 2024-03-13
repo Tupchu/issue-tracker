360c5a56033c314c9a15caf788fd9ec5a46824fe
Tupchu <tupchu5@gmail.com>
2024-03-13 13:07:12 -0500

    Started navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl font-bold">Issue tracker</h1>
-    </main>
+    <div>
+      <h1>Hey</h1>
+    </div>
   );
 }
